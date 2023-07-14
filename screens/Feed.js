377e44d18a796b428afde744850df255b2ee3f4a
brwa kawa <mirzabrwa@gmail.com>
2023-07-14 18:38:15 +0300

    .

--- screens/Feed.js
+++ screens/Feed.js
@@ -1,6 +1,7 @@
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons'; 
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 
 import Home from "./bottomTab/Home";
 import Chat from './bottomTab/Chat';
@@ -29,8 +30,36 @@ export default function Feed() {
                 // tabBarLabel:"Settings"
               }}
             />
-            <BottomTab.Screen name="Chat" component={Chat} />
-            <BottomTab.Screen name="Settings" component={Settings} />
+            <BottomTab.Screen name="Chat" component={Chat} 
+                options={{
+                tabBarIcon: ({ focused })=>{
+                    return (
+                        <MaterialCommunityIcons 
+                          name={focused ? "chat" : "chat-outline"} 
+                          size={26} 
+                          color={focused ? "#48b095" : "grey"} />
+                    )
+                },
+                tabBarActiveTintColor:"#48b095",
+                tabBarInactiveTintColor:"grey",
+                // tabBarLabel:"Settings"
+              }}
+            />
+            <BottomTab.Screen name="Settings" component={Settings} 
+              options={{
+              tabBarIcon: ({ focused })=>{
+                  return (
+                      <Ionicons 
+                        name={focused ? "setting" : "settings-outline"} 
+                        size={26} 
+                        color={focused ? "#48b095" : "grey"} />
+                  )
+              },
+              tabBarActiveTintColor:"#48b095",
+              tabBarInactiveTintColor:"grey",
+              // tabBarLabel:"Settings"
+            }}
+            />
         </BottomTab.Navigator>
   );
 }
