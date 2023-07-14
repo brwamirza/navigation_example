ae7237b6af294bcd5170d3eb402a7e79b71606ae
brwa kawa <mirzabrwa@gmail.com>
2023-07-14 18:31:13 +0300

    .

--- screens/Feed.js
+++ screens/Feed.js
@@ -1,5 +1,6 @@
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons'; 
 
 import Home from "./bottomTab/Home";
 import Chat from './bottomTab/Chat';
@@ -12,10 +13,22 @@ const BottomTab = createBottomTabNavigator();
 
 export default function Feed() {
   return (
-        <BottomTab.Navigator screenOptions={{
-          headerShown: false,
-        }}>
-            <BottomTab.Screen name="Home" component={Home} />
+        <BottomTab.Navigator screenOptions={{headerShown:false}}>         
+            <BottomTab.Screen name="Home" component={Home} 
+               options={{
+                tabBarIcon: ({ focused })=>{
+                    return (
+                        <Ionicons 
+                          name={focused ? "home" : "home-outline"} 
+                          size={26} 
+                          color={focused ? "#48b095" : "grey"} />
+                    )
+                },
+                tabBarActiveTintColor:"#48b095",
+                tabBarInactiveTintColor:"grey",
+                // tabBarLabel:"Settings"
+              }}
+            />
             <BottomTab.Screen name="Chat" component={Chat} />
             <BottomTab.Screen name="Settings" component={Settings} />
         </BottomTab.Navigator>
