ccc9780b0fab4197ac6b07e032cfe1c2dcb02242
brwa kawa <mirzabrwa@gmail.com>
2023-07-16 22:24:34 +0300

    .

--- screens/bottomTab/Chat.js
+++ screens/bottomTab/Chat.js
@@ -1,4 +1,4 @@
-import { SafeAreaView,StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView,ScrollView,StyleSheet, Text, View } from 'react-native';
 import useFetch from '../../api/getColors';
 import {useEffect} from 'react';
 import { ListItem } from '@rneui/themed';
@@ -11,16 +11,18 @@ export default function Chat() {
   console.log(data)
   return (
     <SafeAreaView>
-    {data.map((color)=>{
-      return  <ListItem>
-      <ListItem.Content>
-        <ListItem.Title>{color.color_id}</ListItem.Title>
-        <ListItem.Subtitle>EN: {color.color_en}</ListItem.Subtitle>
-        <ListItem.Subtitle>KU: {color.color_ku}</ListItem.Subtitle>
-        <ListItem.Subtitle>AR: {color.color_ar}</ListItem.Subtitle>
-      </ListItem.Content>
-    </ListItem>
-    })}
+      <ScrollView>
+      {data.map((color)=>{
+          return  <ListItem>
+                    <ListItem.Content>
+                      <ListItem.Title>{color.color_id}</ListItem.Title>
+                      <ListItem.Subtitle>EN: {color.color_en}</ListItem.Subtitle>
+                      <ListItem.Subtitle>KU: {color.color_ku}</ListItem.Subtitle>
+                      <ListItem.Subtitle>AR: {color.color_ar}</ListItem.Subtitle>
+                    </ListItem.Content>
+                  </ListItem>
+        })}
+      </ScrollView>
     </SafeAreaView>
   );
 }
