eb5c07d474ffacfdfed56f3cfc27677069194b5f
brwa kawa <mirzabrwa@gmail.com>
2023-07-16 22:03:46 +0300

    .

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import "react-native-gesture-handler"
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-
+import { I18nManager } from "react-native";
+I18nManager.forceRTL(false);
+I18nManager.allowRTL(false);
 import Feed from "./screens/Feed";
 import Article from "./screens/Article";
 
