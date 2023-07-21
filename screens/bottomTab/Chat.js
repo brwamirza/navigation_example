0f46c9f800fc2ead1915bd77cbd7d3f8398471b1
brwa kawa <mirzabrwa@gmail.com>
2023-07-21 12:25:29 +0300

    .

--- screens/bottomTab/Chat.js
+++ screens/bottomTab/Chat.js
@@ -1,17 +1,23 @@
 import { SafeAreaView,ScrollView,StyleSheet, Text, View } from 'react-native';
 import useFetch from '../../api/getColors';
-import {useEffect} from 'react';
+import {useEffect,useState} from 'react';
 import { FAB ,Dialog, ListItem } from '@rneui/themed';
-
+import { Modal, Portal, Button, PaperProvider } from 'react-native-paper';
 
 
 export default function Chat() {
 
   const {data,error,isLoading} = useFetch();
+  const [visible, setVisible] = useState(false);
+
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+  const containerStyle = {backgroundColor: 'white', padding: 20,};
 
   console.log(data)
   return (
-    <SafeAreaView>
+    <>
+    {/* <SafeAreaView> */}
       <ScrollView>
 
       {(isLoading===false) ?data.map((color)=>{
@@ -29,12 +35,26 @@ export default function Chat() {
       </ScrollView>
       <FAB
         visible={true}
-       onPress={() => console.log("worked")}
+       onPress={showModal}
         placement="right"
         title="+ Add"
         // icon={{ name: 'edit', color: 'white' }}
         color="green"
       />
-    </SafeAreaView>
+
+
+
+    {/* </SafeAreaView> */}
+    <PaperProvider>
+        <Portal>
+          <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
+            <Text>Example Modal.  Click outside this area to dismiss.</Text>
+          </Modal>
+        </Portal>
+        <Button style={{marginTop: 30}} onPress={showModal}>
+          Show
+        </Button>
+    </PaperProvider>
+    </>
   );
 }
